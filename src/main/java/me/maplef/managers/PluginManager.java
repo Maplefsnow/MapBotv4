13976ce5613b2f10a6e93bf0e655b588c352de8f
Maplef <1329785932@qq.com>
2021-12-15 22:59:40 +0800

    不再使用全类名反射，只要继承了接口即可

--- src/main/java/me/maplef/managers/PluginManager.java
+++ src/main/java/me/maplef/managers/PluginManager.java
@@ -1,47 +1,48 @@
 package me.maplef.managers;
 
+import me.maplef.MapbotPlugin;
 import me.maplef.exceptions.CommandNotFoundException;
 import me.maplef.exceptions.InvalidSyntaxException;
 import net.mamoe.mirai.message.data.At;
 import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.MessageChainBuilder;
+import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.SQLException;
 import java.util.Map;
+import java.util.Set;
 
 public class PluginManager {
-    private static final String[] pluginsName = {"Hitokoto", "CatImage", "CheckTPS", "CheckMoney", "CheckOnlineTime", "Weather", "ListPlayers",
-            "BindIDAndQQ", "CheckLocation", "StopServer", "WorldNews", "ReloadConfigs"};
-
     public static MessageChain commandHandler(String command, Long groupID, Long senderID , String[] args) throws Exception{
+        Reflections reflections = new Reflections("me.maplef.plugins");
+        Set<Class<? extends MapbotPlugin>> pluginClasses = reflections.getSubTypesOf(MapbotPlugin.class);
+
         switch (command){
             case "help": case "菜单": {
                 MessageChainBuilder helpMsg = new MessageChainBuilder();
                 helpMsg.append(new At(senderID)).append(" 你好，以下是我支持的命令\n(其中\"<>\"内的为必填参数，\"[]\"内的为可选参数)\n——————\n");
-                for(String pluginName : pluginsName) {
-                    Class<?> clazz = Class.forName("me.maplef.plugins." + pluginName);
-                    Object classObj = clazz.getDeclaredConstructor().newInstance();
-                    Map<String, Object> pluginInfo = (Map<String, Object>) clazz.getMethod("register").invoke(classObj);
+
+                for(Class<? extends MapbotPlugin> singleClass : pluginClasses){
+                    Map<String, Object> pluginInfo = (Map<String, Object>) singleClass.getMethod("register").invoke(singleClass.getDeclaredConstructor().newInstance());
                     Map<String, String> pluginUsage = (Map<String, String>) pluginInfo.get("usages");
 
                     for (Map.Entry<String, String> entry : pluginUsage.entrySet())
                         helpMsg.append(entry.getValue()).append("\n");
                 }
+
                 return helpMsg.build();
             }
 
             default: {
-                for(String pluginName : pluginsName){
-                    Class<?> clazz = Class.forName("me.maplef.plugins." + pluginName);
-                    Object classObj = clazz.getDeclaredConstructor().newInstance();
-                    Map<String, Object> pluginInfo = (Map<String, Object>) clazz.getMethod("register").invoke(classObj);
-
+                for(Class<? extends MapbotPlugin> singleClass : pluginClasses){
+                    Map<String, Object> pluginInfo = (Map<String, Object>) singleClass.getMethod("register").invoke(singleClass.getDeclaredConstructor().newInstance());
                     Map<String, Method> pluginCommands = (Map<String, Method>) pluginInfo.get("commands");
+
                     if(pluginCommands.containsKey(command)){
                         try{
-                            return (MessageChain) pluginCommands.get(command).invoke(classObj, groupID, senderID, args);
+                            return (MessageChain) pluginCommands.get(command).invoke(singleClass.getDeclaredConstructor().newInstance(), groupID, senderID, args);
                         } catch (InvocationTargetException e){
                             MessageChainBuilder errorMsg = new MessageChainBuilder();
                             errorMsg.append(new At(senderID)).append(" ");
@@ -66,15 +67,3 @@ public class PluginManager {
         throw new CommandNotFoundException();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-

196adeb027c9a4d21fd05cef426a7a76b9261006
Maplef <1329785932@qq.com>
2022-07-06 18:44:21 +0800

    优化登录

--- src/main/java/me/maplef/mapbotv4/utils/BotOperator.java
+++ src/main/java/me/maplef/mapbotv4/utils/BotOperator.java
@@ -7,16 +7,23 @@ import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.network.WrongPasswordException;
 import net.mamoe.mirai.utils.BotConfiguration;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 
+import java.io.File;
 import java.util.Objects;
 
 public class BotOperator {
     private static Bot bot;
+    static final FileConfiguration config = Main.getInstance().getConfig();
 
     public static void login(Long qq, String password) throws WrongPasswordException{
+        File logPath = new File(Main.getInstance().getDataFolder(), "logs");
+
         bot = BotFactory.INSTANCE.newBot(qq, password, new BotConfiguration(){{
-            noNetworkLog();
-            noBotLog();
+            redirectNetworkLogToDirectory(logPath);
+            redirectBotLogToDirectory(logPath);
+            setProtocol(MiraiProtocol.valueOf(config.getString("bot-login-device", "ANDROID_PHONE")));
+            setCacheDir(new File("cache"));
         }});
         bot.login();
     }
